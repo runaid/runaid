5ad774dcab3b10caf257e54ec0c81ca3e35839e1
Jakub Kozłowski <kubukoz@gmail.com>
2020-01-26 22:29:50 +0100

    Update flawless

--- tests/src/main/scala/io/runaid/tests/Tests.scala
+++ tests/src/main/scala/io/runaid/tests/Tests.scala
@@ -4,16 +4,13 @@ import cats.effect.ExitCode
 import cats.effect.IO
 import cats.effect.IOApp
 import flawless.TestApp
-import flawless.SuiteClass
-import flawless.data.Suite
-import flawless.dsl._
-import flawless.predicates.all._
-import cats.implicits._
+import flawless.syntax._
 import flawless._
+import cats.implicits._
 
 object Tests extends IOApp with TestApp {
   override def run(args: List[String]): IO[ExitCode] = runTests(args) {
-    RunaidTests.runSuite.toSuites[IO]
+    RunaidTests.runSuite.widenF[IO]
   }
 }
 
